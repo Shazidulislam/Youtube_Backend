d569362fa3f7f638f3fad2a2d8c6ebacba3ce9ec
Shazidulislam <shazidulislam910@gmail.com>
2026-07-26 22:36:58 +0600

    create upload video , thumuile

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -7,28 +7,66 @@ const getAllVideo = asynHandler(async (req, res) => {
   //TODO: get all videos based on query, sort, pagination
 });
 
+
 const publishAVideo = asynHandler(async (req, res) => {
+  const { title, description } = req.body;
   // TODO: get video, upload to cloudinary, create video
-});
 
-const getVideoById = asynHandler(async (req, res) => {
-  const { videoId } = req.params;
-  //TODO: get video by id
+  // work flow
+  //at frist  title , description take from body
+  if (!title || !description) {
+    throw new ApiError(400, "video title and description is required");
+  }
+  //take video  and thambuile file from req.files
+  const videoFileLocalPath = req.files?.videoFile?.[0]?.path;
+  const thumbuileFileLocalPath = req.files?.thambuile?.[0]?.path;
 
-  //   if id is not exists then give error
-  if (!videoId) {
-    throw new ApiError(400, "Video id is required");
+  if (!videoFileLocalPath || !thumbuileFileLocalPath) {
+    throw new ApiError(400, "Video and thumbile file are required.");
   }
 
-  //   find a video use id
-  const video = await Video.findById(videoId);
+  //  now video and thumbuile file uploadOnCloudinary and take url
+  //and upload on uploadOnCloudinary
 
-  //   video not found
-  if (!video) {
-    throw new ApiError(404, "Video not found");
+  const videofile = await uploadOnCloudinary(videoFileLocalPath);
+  const tumbuilefile = await videoFileLocalPath(thumbuileFileLocalPath);
+
+  console.log(videofile)
+
+  // if file not found the give error
+  if (!videofile?.url) {
+    throw new ApiError(
+      500,
+      "Encountered an error while uploading the video file to Cloudinary.",
+    );
   }
+
+  if (!tumbuilefile?.url) {
+    throw new ApiError(
+      500,
+      "Encountered an error while uploading the thumbuile file to Cloudinary.",
+    );
+  }
+
+  // create entry on Mongodb
+  // reamber  on route use veryfyJwt for found owner
+  const video = await Video.create({
+    title,
+    description,
+    videoFile: videofile?.url,
+    duration:videofile?.duration,
+    thumbnail: tumbuilefile?.url,
+    owner: req?.user?._id,
+  });
 
   return res
-    .status(200)
-    .json(new ApiResponse(200, video, "Video fetched successfully"));
+    .status(201)
+    .json(new ApiError(200, video, "Video uploaded successfully."));
 });
+
+
+
+
+const getVideoById = asynHandler(async(req , res)=>{
+  
+})
